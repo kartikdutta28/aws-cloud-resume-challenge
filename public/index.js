2b66820c75f5af226e3d0c02978f377096f06f2f
Kartike Dutta <kartikedutta@Kartikes-MacBook-Air.local>
2024-08-08 17:21:13 +0530

    first commit

---
+++ public/index.js
@@ -0,0 +1,10 @@
+async function updateCounter() {
+    const response = await fetch('https://5e4hezs4pggubt7kwptosrjaly0bqkqx.lambda-url.ap-south-1.on.aws/');
+    console.log('Calling API');
+    let counterDOMElement = document.querySelector('.counter-number');
+    const data = await response.json();
+    console.log(data);
+    counterDOMElement.innerHTML = `Views ${data}`;
+}
+
+updateCounter();
